a96f6fcedcd688e9dcc65da6101c3d0c2eb8efeb
Karl Svartholm <gorbiz@gmail.com>
2016-02-19 05:46:07 +0100

    POC

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var http = require('http');
-var GitHubApi = require('github');
 var jade = require('jade');
+var stylus = require('stylus');
+var nib = require('nib');
+
+var GitHubApi = require('github');
 
 const PORT = 3000;
 const PRETTY = true;
@@ -8,45 +11,71 @@ const PRETTY = true;
 var github = new GitHubApi({version: '3.0.0'});
 
 var render = {
-  jade: function(content) {
-    var html = jade.compile(content, {pretty: PRETTY})();
+  jade: function(str) {
+    var html = jade.compile(str, {pretty: PRETTY})();
     return html;
   }
-  ,html: function(content) {
-    return content;
+  ,html: function(str) { return str; }
+  ,js:   function(str) { return str; }
+  ,css:  function(str) { return str; }
+
+  ,styl: function(str, cb) {
+
+    stylus(str)
+      .set('filename', 'derp.css')
+      .use(nib())
+      .render(function(err, css) {
+        return cb(css);
+      });
   }
 };
 
 var gist = {};
 function handleRequest(req, res) {
+
+  function notFound() {
+    res.writeHead(404);
+    res.end();
+  }
+
   var id = req.url.split('/').pop();
+
   if (/^[0-9a-f]{20}$/.test(id)) {
     console.log('fetching gist: ' + id);
     github.gists.get({id: id}, function(err, gistJson) {
       if (err) throw err;
       gist = gistJson;
 
+      // consider looking for index.* then: if (render[ext]) ...
       var file = gist.files['index.jade'] || gist.files['index.html'];
       if (!file) throw new Error('found no index');
 
       var ext  = file.filename.split('.').pop();
-      var html = render[ext](file.content);
-
       res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-      return res.end(html);
+      res.end(render[ext](file.content));
     });
+
   } else if(/\.js$/.test(id)) {
-    console.log('TODO: handle JS request');
-    res.writeHead(404);
-    return res.end();
+    if (!gist.files || !gist.files[id]) return notFound();
+
+    var file = gist.files[id];
+    var ext = file.filename.split('.').pop();
+    res.writeHead(200, {'Content-Type': 'application/javascript; charset=utf-8'});
+    res.end(render[ext](file.content));
+
   } else if(/\.css$/.test(id)) {
-    console.log('TODO: handle CSS request');
-    res.writeHead(404);
-    return res.end();
+    var base = id.match(/^(.+)\.css$/)[1];
+    var file = gist.files[base + '.styl'] || gist.files[base + '.css'];
+    if (!id) return notFound();
+
+    var ext = file.filename.split('.').pop();
+    render[ext](file.content, function(css) {
+      res.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'});
+      res.end(css);
+    });
+
   } else {
-    console.log('ignoring request: ' + id);
-    res.writeHead(404);
-    return res.end();
+    notFound();
   }
 }
 
