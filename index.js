5fb40d24fec294c26e074dfa9abfcf56f79420e8
Karl Svartholm <gorbiz@gmail.com>
2016-02-28 02:07:44 +0100

    :art: refactored → NiCE!

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var path = require('path');
 var jade = require('jade');
 var stylus = require('stylus');
 var nib = require('nib');
@@ -11,18 +10,23 @@ const PRETTY_RENDER = true;
 
 var github = new GitHubApi({version: '3.0.0'});
 
-var render = {
-  jade: function(str) {
-    str = jade.compile(str, {pretty: false || PRETTY_RENDER})();
-    return str;
-  }
-  ,styl: function(str) {
-    return stylus(str)
-      .set('filename', 'derp.css')
-      .use(nib())
-      .render();
-  }
-};
+var transformers = [{
+      input: /(.+)(\.jade)/
+    , output: 'html'
+    , type: 'text/html'
+    , transform: function(str) {
+        return jade.compile(str, {pretty: PRETTY_RENDER || false})();
+    }}
+  , {
+      input: /(.+)(\.styl)/
+    , output: 'css'
+    , type: 'text/css'
+    , transform: function(str) {
+        return stylus(str)
+          .use(nib())
+          .render();
+    }
+}];
 
 var app = express();
 
@@ -41,43 +45,28 @@ app.get('/gists/:id/*', function (req, res, next) {
   });
 });
 
-app.get('/gists/:id/', function (req, res) {
-  var file = req.gist.files['index.jade'] || req.gist.files['index.html'];
-  if (!file) throw new Error('found no index');
-  var ext  = file.filename.split('.').pop();
-  res.send(render[ext](file.content));
-});
-
-var contentType = {js: 'application/javascript', css: 'text/css'};
-
-// first look for exactly the file requested (same file extension)
-app.get('/gists/:id/:file', function (req, res, next) {
-  if (!req.gist.files[req.params.file]) return next();
-  var   file = req.params.file
-      , ext = file.split('.').pop()
-      , content = req.gist.files[file].content;
-  if (!render[ext]) return res.end(content);
-  render[ext](content, function(transformed) {
-    if (contentType[ext]) res.writeHead(200, {'Content-Type': contentType[ext] + '; charset=utf-8'});
-    res.end(content);
+// pre-render all transpilable files
+app.get('/gists/:id/*', function (req, res, next) {
+  Object.keys(req.gist.files).map(function(file) {
+    transformers.filter(function(t) { return t.input.test(file);
+    }).map(function(t) { // should really only be CALLED ONCE (or 0 times)
+      var newFile = t.input.exec(file)[1] + '.' + t.output; // Yeah, we might overwrite an existing one; fuck it!
+      req.gist.files[newFile] = {
+        content: t.transform(req.gist.files[file].content),
+        type: t.type
+      };
+    });
   });
+  next();
 });
 
-app.get('/gists/:id/:file', function (req, res, next) {
-  var urlPath = path.parse(req.params.file);
-  if (urlPath.ext) urlPath.ext = urlPath.ext.substr(1); // remove prefix-dot
-  var exts = ({html: ['jade'], css: ['styl']})[urlPath.ext];
-  for (var i in exts) {
-    var ext = exts[i];
-    var file = req.gist.files[urlPath.name + '.' + ext];
-    if (!file) continue;
-    var content = render[ext](file.content);
-    if (contentType[urlPath.ext]) res.writeHead(200, {'Content-Type': contentType[urlPath.ext] + '; charset=utf-8'});
-    res.end(content);
-  }
-  next('Could not handle: ' + req.params.file);
+app.get('/gists/:id/:file?', function (req, res, next) {
+  if (!req.params.file) req.params.file = 'index.html';
+  if (!req.gist.files[req.params.file]) return next();
+  var file = req.gist.files[req.params.file];
+  res.writeHead(200, {'Content-Type': file.type + '; charset=utf-8'});
+  return res.end(file.content);
 });
-
 
 app.listen(PORT, function () {
   console.log('Server listening on: http://localhost:%s', PORT);
