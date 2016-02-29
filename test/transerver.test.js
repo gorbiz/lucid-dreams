20902b5ed17621a7d4c4947860482609ec1c1bb4
Karl Svartholm <gorbiz@gmail.com>
2016-02-29 03:08:05 +0100

    improve test names

--- test/transerver.test.js
+++ test/transerver.test.js
@@ -4,15 +4,15 @@ var transerver = require('../lib/transerver.js');
 
 describe('transerver', function(){
 
-  describe('basic', function(){
-    it('should serve html', function(){
+  describe('serves', function(){
+    it('html', function(){
       var t = transerver({files: {
         'index.html': {content: '<h1>HAI!</h1>'}
       }});
       assert.equal('<h1>HAI!</h1>', t.get('index.html'));
     });
 
-    it('should serve multiple files', function(){
+    it('multiple files', function(){
       var t = transerver({files: {
         'index.html': {content: '<h1>HAI!</h1>'},
         'menu.html': {content: '<nav>menu</nav>'}
@@ -21,21 +21,21 @@ describe('transerver', function(){
       assert.equal('<nav>menu</nav>', t.get('menu.html'));
     });
 
-    it('should serve jade', function(){
+    it('jade', function(){
       var t = transerver({files: {
         'index.jade': {content: 'h1 HAI!'}
       }});
       assert.equal('<h1>HAI!</h1>', t.get('index.html'));
     });
 
-    it('should serve stylus', function(){
+    it('stylus', function(){
       var t = transerver({files: {
         'style.styl': {content: 'body\n  padding: 50px'}
       }});
       assert.equal('body {\n  padding: 50px;\n}\n', t.get('style.css'));
     });
 
-    it('should serve stylus with nib', function(){
+    it('stylus with nib', function(){
       var t = transerver({files: {
         'style.styl': {content:
             '@import nib\n'+
@@ -50,6 +50,7 @@ describe('transerver', function(){
         t.get('style.css'));
     });
 
-    it('should serve stylus with that other thing available @ CodePen...');
+    it('jade with includes');
+
   });
 });
