966cd09e813778d767f4981da5e077c3502a69d3
Karl Svartholm <gorbiz@gmail.com>
2016-09-17 21:04:18 +0200

    support commit hash, close #10 & allow basic :octocat: GitHub auth
    Use basic :octocat: GitHub auth:
    ```sh
    GIT_USER=username GIT_PASS=password npm start
    ```

--- README.md
+++ README.md
@@ -6,14 +6,18 @@ Supported forms:
 ```
 http://host/GIST_ID/[filename]
 http://host/USER/REPO/[filename]
+http://host/USER/REPO/commit/SHA/[filename]
 ```
 
 ## Examples
 https://gist.github.com/gorbiz/252f5c6ac19879dfbda9  
 → http://gorbiz.com/gists/252f5c6ac19879dfbda9/
 
-https://github.com/gorbiz/latin-book  
-→ http://gorbiz.com/gorbiz/latin-book/
+https://github.com/gorbiz/touchart  
+→ http://gorbiz.com/gorbiz/touchart
+
+https://github.com/gorbiz/touchart/commit/c4dd0995213a18a9dffc029906537d308988bb5a  
+→ http://gorbiz.com/gorbiz/touchart/commit/c4dd0995213a18a9dffc029906537d308988bb5a
 
 PS.
 This is running on *gorbiz.com*.
--- index.js
+++ index.js
@@ -6,6 +6,12 @@ var async = require('async');
 
 const PORT = process.env.PORT || 3000;
 var github = new GitHubApi({version: '3.0.0'});
+github.authenticate({
+    type: 'basic',
+    username: 'gorbiz',
+    password: '***REMOVED***'
+});
+
 var app = express();
 
 var cache = {};
@@ -53,16 +59,19 @@ app.get('/gists/:id/:file?', transerverServer);
 // repos
 
 // trailing slash; paramount to get relatvie resources (like a script.js) right
-app.get('/:user/:repo', trailingSlash);
-app.get('/:user/:repo/*', function (req, res, next) {
+app.get(['/:user/:repo/commit/:sha', '/:user/:repo'], trailingSlash);
+app.get(['/:user/:repo/commit/:sha/*', '/:user/:repo/*'], function (req, res, next) {
   var user = req.params.user, repo = req.params.repo;
-  // ex https://api.github.com/repos/gorbiz/latin-book/commits
-  github.repos.getCommits({user: user, repo: repo}, function(err, commits) {
-    if (err) return next(err);
-    if (!commits.length) return res.status(204).send('No Content');
-    // ex https://api.github.com/repos/gorbiz/latin-book/git/trees/70e964e8dadbc84eb5250648b70f53c82d046318
 
-    var sha = commits[0].sha;
+  (function (cb) {
+    if (req.params.sha) return cb(req.params.sha);
+    github.repos.getCommits({user: user, repo: repo}, function(err, commits) {
+      if (err) return next(err);
+      if (!commits.length) return res.status(204).send('No Content');
+      // ex https://api.github.com/repos/gorbiz/latin-book/git/trees/70e964e8dadbc84eb5250648b70f53c82d046318
+      cb(commits[0].sha);
+    });
+  })(function (sha) {
     if (cache[user+'#'+repo+'#'+sha]) {
       // TODO check cache headers...
       console.log('loading ' + user+'/'+repo+'#'+sha + ' from cache.');
@@ -70,33 +79,30 @@ app.get('/:user/:repo/*', function (req, res, next) {
       return next();
     }
 
-    github.gitdata.getTree({ user: user, repo: repo, sha: sha, recursive: true
-      }, function(err, tree) {
-          if (err) return next(err);
-
-          var files = {};
-          async.map(tree.tree,
-              function(node, cb) {
-                if (node.type == 'tree') return cb(); // don't need folders
-                if (node.type != 'blob') cb(new Error('Not sure what to do with a "' + node.type + '"'));
-                node.filename = node.path;
-                // ex https://api.github.com/repos/gorbiz/latin-book/git/blobs/a25df821ae7ac0f135d9f455b9987457f1d4e01f
-                github.gitdata.getBlob({user: user, repo: repo, sha: node.sha}, function(err, blob) {
-                  if (err) return cb(err);
-                  node.content = '' + new Buffer(blob.content, 'base64');
-                  files[node.path] = node;
-                  cb();
-                });
-              }, function(err) {
-                if (err) return next(err);
-                req.transerver = cache[user+'#'+repo+'#'+sha] = transerver({files: files});
-                next();
-              });
+    github.gitdata.getTree({ user: user, repo: repo, sha: sha, recursive: true }, function(err, tree) {
+      if (err) return next(err);
+      var files = {};
+      async.map(tree.tree, function(node, cb) {
+        if (node.type == 'tree') return cb(); // don't need folders
+        if (node.type != 'blob') cb(new Error('Not sure what to do with a "' + node.type + '"'));
+        node.filename = node.path;
+        // ex https://api.github.com/repos/gorbiz/latin-book/git/blobs/a25df821ae7ac0f135d9f455b9987457f1d4e01f
+        github.gitdata.getBlob({user: user, repo: repo, sha: node.sha}, function(err, blob) {
+          if (err) return cb(err);
+          node.content = '' + new Buffer(blob.content, 'base64');
+          files[node.path] = node;
+          cb();
+        });
+      }, function(err) {
+        if (err) return next(err);
+        req.transerver = cache[user+'#'+repo+'#'+sha] = transerver({files: files});
+        next();
       });
+    });
   });
 });
 
-app.get('/:user/:repo/:file(*)', transerverServer);
+app.get(['/:user/:repo/commit/:sha/:file(*)', '/:user/:repo/:file(*)'], transerverServer);
 
 app.listen(PORT, function () {
   console.log('Server listening on: http://localhost:%s', PORT);
